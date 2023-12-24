cc627f8cf56e4bc3ed1153d5c011ddb8d8b2d1a3
Daniel Yuki <danielyukihiga1@gmail.com>
2023-12-24 17:49:53 -0300

    feat: nightLights(poorly),clouds, bumpMap, oceanMap

--- src/Components/Earth.jsx
+++ src/Components/Earth.jsx
@@ -14,41 +14,48 @@ import atmosphereFragmentShader from "../assets/Shaders/atmosphereFragment.glsl"
 
 const Earth = () => {
     const earthTexture = useLoader(TextureLoader, "/EarthTexture.jpg");
+    const earthBumpMap = useLoader(TextureLoader, "/EarthBumpMap.jpg");
+    const earthTextureNight = useLoader(TextureLoader, "/EarthTextureNight.jpg");
+    const oceanMap = useLoader(TextureLoader, "/OceanMapV3.jpg");
+
     const earthRef = useRef();
     const groupRef = useRef();
-
+    
     const tiltAngle = -(23.5 * (Math.PI / 180));
-
+    
     const mouseCoords = {
         x: 0,
         y: 0,
     };
-
+    
     addEventListener("mousemove", (e) => {
         mouseCoords.x = (e.clientX / innerWidth) * 2 - 1;
         mouseCoords.y = -(e.clientY / innerHeight) * 2 + 1;
     });
-
+    
     useFrame((state, delta) => {
-        groupRef.current.rotation.y += (delta / 10)
-
+        earthRef.current.rotation.y += delta / 10;
+        
         const elapsedTime = state.clock.getElapsedTime();
-        groupRef.current.rotation.x = (tiltAngle * Math.sin(elapsedTime/10) * 0.8);
-
-        gsap.to(earthRef.current.rotation, {
+        earthRef.current.rotation.x =
+        tiltAngle * Math.sin(elapsedTime / 10) * 0.8;
+        
+        gsap.to(groupRef.current.rotation, {
             y: mouseCoords.x * 0.3,
             x: -mouseCoords.y * 0.2,
             duration: 2,
         });
     });
-
+    
     return (
-        <group ref={groupRef} >
-            <mesh ref={earthRef} >
+        <group ref={groupRef}>
+            <mesh ref={earthRef}
+                castShadow
+                >
                 {/* <axesHelper args={[2]} /> */}
-                <sphereGeometry args={[1, 64, 64]} attach="geometry"/>
+                <sphereGeometry args={[1, 64, 64]} attach="geometry" />
 
-                <shaderMaterial
+                {/* <shaderMaterial
                     attach="material"
                     vertexShader={earthVertexShader}
                     fragmentShader={earthFragmentShader}
@@ -56,23 +63,54 @@ const Earth = () => {
                         uTexture: { value: earthTexture },
                     }}
                     lights={false}
-                />
+                /> */}
 
-                {/* <CustomShaderMaterial
-                baseMaterial={MeshPhysicalMaterial}
-                vertexShader={earthVertexShader}
-                fragmentShader={earthFragmentShader}
-                uniforms={{
-                    uTexture: { value: earthTexture },
-                }}
-                roughness={0.95}
-                metalness={0.25}
-                // silent
-            /> */}
+                <CustomShaderMaterial
+                    baseMaterial={MeshPhysicalMaterial}
+                    vertexShader={earthVertexShader}
+                    fragmentShader={earthFragmentShader}
+                    uniforms={{
+                        uTexture: { value: earthTexture },
+                    }}
+                    silent
+                    bumpMap={earthBumpMap}
+                    bumpScale={2}
+                    roughnessMap={oceanMap}
+                    // roughness={0.5}
+                    metalnessMap={oceanMap}
+                    metalness={0.25}
+                    emissiveMap={earthTextureNight}
+                    emissive={0xffff88}
+                    emissiveIntensity={.35}
+                    />
             </mesh>
+            <Clouds />
         </group>
     );
 };
+
+const Clouds = () => {
+    const cloudsTexture = useLoader(TextureLoader, "/CloudMap.jpg");
+    const cloudsRef = useRef();
+
+    useFrame((state, delta) => {
+        cloudsRef.current.rotation.y += delta / 8.5;
+    });
+    
+    return (
+        <mesh ref={cloudsRef}>
+            <sphereGeometry args={[1.01, 64, 64]} attach="geometry" />
+            <meshStandardMaterial
+                attach="material"
+                transparent
+                opacity={0.25}
+                depthWrite={false}
+                map={cloudsTexture}
+                blending={2}
+            />
+        </mesh>
+    );
+}
 
 const Atmosphere = () => {
     return (
@@ -97,15 +135,15 @@ export default function EarthCanva() {
             gl={{ preserveDrawingBuffer: true }}
         >
             <Suspense fallback={<CanvasLoader />}>
-                {/* <ambientLight intensity={0.1} color={[0.6, 0.8, 1]} />
+                <ambientLight intensity={0.15} color={[0.6, 0.8, 1]} />
                 <directionalLight
-                    castShadow
                     position={[100, 0, 100]}
                     intensity={2.5}
-                    shadow-mapSize-width={1024}
-                    shadow-mapSize-height={1024}
-                    color={[0.75, 0.8, 1]}
-                /> */}
+                    color={[0.8, 0.8, 1]}
+                    // castShadow
+                    // shadow-mapSize-width={1024}
+                    // shadow-mapSize-height={1024}
+                />
                 <Earth />
                 <Atmosphere />
             </Suspense>
